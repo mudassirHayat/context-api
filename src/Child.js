a3081d4f723e88b80c2d5a2126ff456569850035
mudassirHayat <64309904+mudassirHayat@users.noreply.github.com>
2020-06-16 17:11:39 +0500

    without context api
    if we send data from parent to grand child it is not passed direct we have also pass it to child  first .. this problem can be solved by using context api

---
+++ src/Child.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Child = (props)=>{
+    return(
+     <div>
+         <h3>my first name is {props.first} </h3>
+         
+     </div>
+    )
+}
+export default Child
